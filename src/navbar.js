824129d22ce23395b2e30ad1b83d4dc8d0fca54e
Nishkarsh <nishkarsh.gupta1036@gmail.com>
2022-05-03 22:09:02 +0530

    unecessary files deleted

--- src/navbar.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".hamburger");
-const navbar = document.querySelector(".navbar");
-
-btn.addEventListener("click", function() {
-    console.log("hii");
-    navbar.classList.toggle("change");
-});
